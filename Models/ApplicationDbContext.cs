ffd718addea512691c3ff72b585cd581ad17f383
tanjina01 <106947215+tanjina01@users.noreply.github.com>
2024-09-02 23:35:40 +0600

    Add files via upload

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApi1.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
